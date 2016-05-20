d7dd453820ef4bc49229b16a32d3890377ade85e
yakkub5 <kubo.kolar@gmail.com>
2016-05-20 16:43:53 +0200

    Merge origin master.

--- www/js/app.js
+++ www/js/app.js
@@ -3,14 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+
 var app = angular.module('starter', [
-  'ionic',
-   'ngRoute',
+    'ionic',
+    'ngRoute',
     'firebase',
     'ui.router',
     'control',
-     'factory',
-      'ui.calendar']);
+    'factory',
+    'ui.calendar'
+]);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -93,19 +95,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
             // controller: "UserController"
         })
         .state('home', {
-    url: '/home',
-    "abstract": true,
-    templateUrl: 'templates/scrHome-content.html'
-   })
-   .state('home.calendar', {
-    url: '/calendar2',
-    views: {
-     'menuContent': {
-      templateUrl: 'templates/scrCalendar.html',
-      controller: 'CalendarController'
-     }
-    }
-   });
+          url: '/home',
+          "abstract": true,
+          templateUrl: 'templates/scrHome-content.html'
+         })
+         .state('home.calendar', {
+          url: '/calendar2',
+          views: {
+           'menuContent': {
+            templateUrl: 'templates/scrCalendar.html',
+            controller: 'CalendarController'
+           }
+          }
+         });
 
-    $urlRouterProvider.otherwise('/main');
+      $urlRouterProvider.otherwise('/main');
 });
+
